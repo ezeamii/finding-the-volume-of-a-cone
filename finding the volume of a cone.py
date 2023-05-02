00ee9611dedcbee6578eae63dea307acb99ef0ba
ezeamii <131669112+ezeamii@users.noreply.github.com>
2023-05-02 12:55:47 +0100

    Add files via upload

---
+++ finding the volume of a cone.py
@@ -0,0 +1,7 @@
+# PROGRAM TO FIND THE VOLUME OF A CONE
+radius = float(input("Put in the radius of the cone: "))
+height = float(input("Put in the height of the cone: "))
+pi = 3.14
+
+Volume = ((1/3) * pi * (radius * radius) * (height))
+print("The volume of this cone is", Volume,"cm^3")
